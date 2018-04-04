d39fcf464de69907e911d2d5351bba747d4d5875
jackdbd <jackdebidda@gmail.com>
2018-04-04 22:48:00 +0200

    Update script to scrape SVG attributes/elements

--- scripts/extract-attributes.js
+++ scripts/extract-attributes.js
@@ -1,7 +1,14 @@
+'use strict';
+
+const fs = require('fs')
 const request = require('request');
 const cheerio = require('cheerio');
 
-const htmlUrl = 'https://developer.mozilla.org/en-US/docs/Web/SVG/Element';
+const htmlUrlElements = 'https://developer.mozilla.org/en-US/docs/Web/SVG/Element';
+const htmlUrlAttributes = 'https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute';
+
+const dataPathElements = './data/elements.json';
+const dataPathAttributes = './data/attributes.txt';
 
 function extractElements($) {
     const $index = $('#SVG_elements_A_to_Z').next('.index');
@@ -19,11 +26,35 @@ function extractElements($) {
     return elements.get();
 }
 
-request(htmlUrl, (error, response, html) => {
+function extractAttributes($) {
+    const $index = $('#SVG_attributes_A_to_Z').next('.index');
+
+    const attributes = $index.find('ul>li>code>a').map((i, row) => {
+        const attribute = row.children[0].data;
+        return attribute;
+    });
+
+    return attributes.get();
+}
+
+request(htmlUrlElements, (error, response, html) => {
   if (error) {
       throw error;
   }
   const $ = cheerio.load(html);
   const elements = extractElements($);
-  console.log(elements);
+  const out = elements;
+    // Print out JSON with 4-space indentation formatting.
+    // http://stackoverflow.com/a/11276104
+    fs.writeFileSync(dataPathElements, JSON.stringify(out, null, 4));
 });
+
+request(htmlUrlAttributes, (error, response, html) => {
+    if (error) {
+        throw error;
+    }
+    const $ = cheerio.load(html);
+    const attributes = extractAttributes($);
+    const out = attributes.join('\n');
+    fs.writeFileSync(dataPathAttributes, out);
+  });
