27c73c528360fb5762d256b867b78bcd54753864
jackdbd <jackdebidda@gmail.com>
2018-04-04 23:52:58 +0200

    Move to a promise-based solution

--- scripts/extract-attributes.js
+++ scripts/extract-attributes.js
@@ -1,60 +1,88 @@
 'use strict';
 
-const fs = require('fs')
+const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
 
-const htmlUrlElements = 'https://developer.mozilla.org/en-US/docs/Web/SVG/Element';
-const htmlUrlAttributes = 'https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute';
-
-const dataPathElements = './data/elements.json';
-const dataPathAttributes = './data/attributes.txt';
+const dataPath = './data/attributes.json';
 
 function extractElements($) {
-    const $index = $('#SVG_elements_A_to_Z').next('.index');
+  const $index = $('#SVG_elements_A_to_Z').next('.index');
 
-    const elements = $index.find('ul>li>a').map((i, row) => {
-        const description = row.attribs.title;
-        const tag = row.children[0].children[0].data;
-        const element = {
-            description,
-            tag
-        }
-        return element;
-    });
+  const elements = $index.find('ul>li>a').map((i, row) => {
+    const description = row.attribs.title;
+    const tag = row.children[0].children[0].data;
+    const element = {
+      description,
+      tag,
+    };
+    return element;
+  });
 
-    return elements.get();
+  return elements.get();
 }
 
 function extractAttributes($) {
-    const $index = $('#SVG_attributes_A_to_Z').next('.index');
+  const $index = $('#SVG_attributes_A_to_Z').next('.index');
 
-    const attributes = $index.find('ul>li>code>a').map((i, row) => {
-        const attribute = row.children[0].data;
-        return attribute;
-    });
+  const attributes = $index.find('ul>li>code>a').map((i, row) => {
+    const attribute = row.children[0].data;
+    return attribute;
+  });
 
-    return attributes.get();
+  return attributes.get();
 }
 
-request(htmlUrlElements, (error, response, html) => {
-  if (error) {
-      throw error;
-  }
-  const $ = cheerio.load(html);
-  const elements = extractElements($);
-  const out = elements;
+function promiseElements(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, html) => {
+      if (error) {
+        throw error;
+      }
+      if (response.statusCode !== 200) {
+        reject(`GET failed: ${response.statusCode}`);
+      }
+      const $ = cheerio.load(html);
+      const elements = extractElements($);
+      resolve(elements);
+    });
+  });
+}
+
+function promiseAttributes(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, html) => {
+      if (error) {
+        throw error;
+      }
+      if (response.statusCode !== 200) {
+        reject(`GET failed: ${response.statusCode}`);
+      }
+      const $ = cheerio.load(html);
+      const attributes = extractAttributes($);
+      resolve(attributes);
+    });
+  });
+}
+
+const promises = [
+  promiseElements('https://developer.mozilla.org/en-US/docs/Web/SVG/Element'),
+  promiseAttributes(
+    'https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute'
+  ),
+];
+
+Promise.all(promises)
+  .then(values => {
+    const [elements, attributes] = values;
+    const out = {
+      elements,
+      attributes,
+    };
     // Print out JSON with 4-space indentation formatting.
     // http://stackoverflow.com/a/11276104
-    fs.writeFileSync(dataPathElements, JSON.stringify(out, null, 4));
-});
-
-request(htmlUrlAttributes, (error, response, html) => {
-    if (error) {
-        throw error;
-    }
-    const $ = cheerio.load(html);
-    const attributes = extractAttributes($);
-    const out = attributes.join('\n');
-    fs.writeFileSync(dataPathAttributes, out);
+    fs.writeFileSync(dataPath, JSON.stringify(out, null, 4));
+  })
+  .catch(err => {
+    console.log(`${err}`);
   });
